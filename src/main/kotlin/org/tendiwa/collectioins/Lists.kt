659943227a7717a7559ae64f5af4fa860ba8f742
Georgy Vlasov <wlasowegor@gmail.com>
2015-11-18 14:52:43 +0500

    Implement List.randomElement

--- src/main/kotlin/org/tendiwa/collectioins/Lists.kt
+++ src/main/kotlin/org/tendiwa/collectioins/Lists.kt
@@ -1,5 +1,7 @@
 package org.tendiwa.collectioins
 
+import java.util.*
+
 fun <T> List<T>.nextAfter(index: Int): T {
     return this[loopedNextIndex(index, size)]
 }
@@ -8,3 +10,9 @@ fun <T> List<T>.prevBefore(index: Int): T {
     return this[loopedPrevIndex(index, size)]
 }
 
+/**
+ * Returns a random element from this list. Mutates `random`.
+ * @param random Source of randomness.
+ */
+fun <T> List<T>.randomElement(random: Random): T =
+    this[Math.floor(this.size * random.nextDouble()).toInt()]
--- src/test/kotlin/org/tendiwa/collections/ListsTest.kt
+++ src/test/kotlin/org/tendiwa/collections/ListsTest.kt
@@ -3,6 +3,8 @@ package org.tendiwa.collections
 import org.junit.Test
 import org.tendiwa.collectioins.nextAfter
 import org.tendiwa.collectioins.prevBefore
+import org.tendiwa.collectioins.randomElement
+import java.util.*
 import kotlin.test.assertEquals
 
 class ListsTest {
@@ -16,5 +18,16 @@ class ListsTest {
         val list = listOf("a", "b", "c")
         assertEquals("c", list.prevBefore(0))
         assertEquals("a", list.prevBefore(1))
+    }
+
+    @Test fun randomElement() {
+        val random: Random = Random(0)
+        val list = listOf("a", "b", "c")
+        assert(
+            (0..100)
+                .map { list.randomElement(random) }
+                .toSet()
+                .containsAll(list)
+        )
     }
 }
