14a60baed27f71a910750d09142cafe6aefc1520
Georgy Vlasov <wlasowegor@gmail.com>
2015-11-19 13:33:19 +0500

    Implement function for searching for the first occurence of an euqal or higher element in a sorter array of ints

--- src/main/kotlin/org/tendiwa/collectioins/Arrays.kt
+++ src/main/kotlin/org/tendiwa/collectioins/Arrays.kt
@@ -9,3 +9,55 @@ fun loopedNextIndex(current: Int, size: Int): Int =
 fun BooleanArray2D(width: Int, height: Int): Array<BooleanArray> =
     Array(width, { BooleanArray(height) })
 
+/**
+ * Finds a sorted array's element that is {@code >= value} for which the
+ * previous element is {@code < value}.
+ *
+ * Works in O(log(n))
+ *
+ * If {@code array} is not sorted, output is undefined.
+ * @param array A sorted array to search in.
+ * @param value A value.
+ * @return Index of an element in {@code array} that is {@code >= value} for
+ * which the previous element is {@code < value}, or -1 iv {@code value} is
+ * greater than all the elements in {@code array}.
+ */
+fun IntArray.findEqualOrHigherInSortedArray(value: Int): Int {
+    var imax = this.size - 1;
+    var imin = 0;
+    var lastGreaterImid = -1;
+    while (imax >= imin) {
+        // TODO: Shouldn't there be parentheses?
+        var imid = imax + imin / 2;
+        if (this[imid] == value) {
+            // TODO: I don't rememeber if method description is correct about
+            // "previous element < value", but if it is, this assertion must be
+            // true.
+            imid = slideBackOverEqualElements(imid)
+            assert(imid == 0 || this[imid - 1] < this[imid])
+            return imid;
+        }
+        if (this[imid] < value) {
+            imin = imid + 1;
+        } else {
+            assert(this[imid] > value)
+            lastGreaterImid = imid;
+            imax = imid - 1;
+        }
+    }
+    // TODO: Maybe it should return array.length instead of -1 when binary search didn't find anything?
+    return lastGreaterImid;
+}
+
+private fun IntArray.slideBackOverEqualElements(lastGreaterImid: Int): Int {
+    var lastGreaterImid1 = lastGreaterImid
+    if (lastGreaterImid1 != -1) {
+        while (previousElementIsTheSame(lastGreaterImid1)) {
+            lastGreaterImid1--;
+        }
+    }
+    return lastGreaterImid1
+}
+
+private fun IntArray.previousElementIsTheSame(lastGreaterImid: Int) =
+    lastGreaterImid > 0 && this[lastGreaterImid - 1] == this[lastGreaterImid]
--- src/test/kotlin/org/tendiwa/collections/ArraysTest.kt
+++ src/test/kotlin/org/tendiwa/collections/ArraysTest.kt
@@ -1,6 +1,7 @@
 package org.tendiwa.collections
 
 import org.junit.Test
+import org.tendiwa.collectioins.findEqualOrHigherInSortedArray
 import org.tendiwa.collectioins.loopedNextIndex
 import org.tendiwa.collectioins.loopedPrevIndex
 import kotlin.test.assertEquals
@@ -25,6 +26,27 @@ class ArraysTest {
         assertEquals(
             3,
             loopedNextIndex(current = 2, size = 4)
+        )
+    }
+
+    @Test fun findEqualOrHigherInSortedArray() {
+        val array = intArrayOf(1, 2, 3, 4, 5, 6, 6, 6, 7, 7, 8, 9, 10)
+        assertEquals(5, array.findEqualOrHigherInSortedArray(6))
+        assertEquals(0, array.findEqualOrHigherInSortedArray(1))
+        assertEquals(array.lastIndex, array.findEqualOrHigherInSortedArray(10))
+    }
+
+    @Test fun findEqualOrHigherInSortedArray_returnsMinus1IfValueIsTooBig() {
+        assertEquals(
+            -1,
+            intArrayOf(1, 2, 3).findEqualOrHigherInSortedArray(4)
+        )
+    }
+
+    @Test fun findEqualOrHigherInSortedAray_findsHigherElement() {
+        assertEquals(
+            1,
+            intArrayOf(1, 3, 4, 6, 10).findEqualOrHigherInSortedArray(2)
         )
     }
 }
